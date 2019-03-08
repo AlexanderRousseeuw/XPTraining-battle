12a68db60f81130b41855f832776aa5b85815c93
Alexander Rousseeuw <Alexander_rousseeuw@hotmail.com>
2019-03-08 14:35:47 +0100

    Fighting armies

--- src/main/java/be/cegeka/battle/Army.java
+++ src/main/java/be/cegeka/battle/Army.java
@@ -16,4 +16,24 @@ public class Army {
     public Set<Soldier> getSoldiers() {
         return this.soldiers;
     }
+
+    public Army fight(Army army) {
+
+        while (this.getSoldiers().size() > 0 && army.getSoldiers().size() > 0) {
+
+            Soldier thisSoldier = this.getSoldiers().stream().findFirst().get();
+            Soldier otherSoldier = army.getSoldiers().stream().findFirst().get();
+            Soldier winner = thisSoldier.fight(otherSoldier);
+
+            if (winner.equals(thisSoldier)) {
+                army.getSoldiers().remove(otherSoldier);
+            } else this.getSoldiers().remove(thisSoldier);
+        }
+
+        if (this.getSoldiers().size() == 0) {
+            return army;
+        }
+
+        return this;
+    }
 }
--- src/test/java/be/cegeka/battle/ArmyTest.java
+++ src/test/java/be/cegeka/battle/ArmyTest.java
@@ -12,4 +12,45 @@ public class ArmyTest {
         army.enlist(soldier);
         Assertions.assertThat(army.getSoldiers()).contains(soldier);
     }
+
+    @Test
+    public void armyCanFightArmy() {
+        Army army = new Army();
+        Army army2 = new Army();
+        Assertions.assertThat(army.fight(army2)).isInstanceOf(Army.class);
+    }
+
+    @Test
+    public void strongestArmyWins() {
+        Army army = new Army();
+        Soldier soldier = new Soldier("Jef", new Axe());
+        army.enlist(soldier);
+
+        Army army2 = new Army();
+        Soldier soldier2 = new Soldier("Jef", new BareFist());
+        army2.enlist(soldier2);
+
+        Assertions.assertThat(army.fight(army2)).isEqualTo(army);
+
+    }
+
+    @Test
+    public void weakestArmyLoses() {
+        Army army = new Army();
+        Soldier soldier = new Soldier("Jef", new Axe());
+        army.enlist(soldier);
+
+        Army army2 = new Army();
+        Soldier soldier2 = new Soldier("Jef", new BareFist());
+        Soldier soldier3 = new Soldier("Jef", new BareFist());
+        Soldier soldier4 = new Soldier("Jef", new BareFist());
+        Soldier soldier5 = new Soldier("Jef", new BareFist());
+        army2.enlist(soldier2);
+        army2.enlist(soldier3);
+        army2.enlist(soldier4);
+        army2.enlist(soldier5);
+
+        Assertions.assertThat(army.fight(army2)).isEqualTo(army);
+
+    }
 }
